8a82889e03067d0c9bb08e2209e0867f964a6d7f
k0rsakov <korsakov.iyu@gmail.com>
2023-08-19 10:10:07 +0300

    Изменен проект scd_dag_factory: - Добавлен скрипт для создания таблицы

--- dag_factory/script_for_generate_data.py
+++ dag_factory/script_for_generate_data.py
@@ -4,6 +4,13 @@ from connectors_to_databases import PostgreSQL
 
 pg = PostgreSQL(port=1)
 
+pg.execute_script(
+    '''
+    CREATE TABLE IF NOT EXISTS fct_some_table_with_random_values
+    ("date" date, "values" int8)
+    '''
+)
+
 date_list = list(pd.date_range(start='2022-01-01', end='2024-01-01'))
 
 values_list = [random.randint(a=1,b=100) for i in range(len(date_list))]
